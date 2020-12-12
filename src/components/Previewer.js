db0c698743f6ea67c098af9fb09b46f87a23033e
Suzette Sousa <sousa.suzette.pro@gmail.com>
2020-12-12 12:03:21 +0100

    add library marked and test

--- src/components/Previewer.js
+++ src/components/Previewer.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import marked from 'marked';
 
 class Previewer extends React.Component {
   render() {
+    const inputMarkdown = '# Markdown';
+    const convertMarkdown = marked(inputMarkdown);
     return (
       <div id="preview">
-
+        <div>{inputMarkdown}</div>
+        <div>{convertMarkdown}</div>
+        <div dangerouslySetInnerHTML={{__html: convertMarkdown}}></div>
       </div>
     )
   }
